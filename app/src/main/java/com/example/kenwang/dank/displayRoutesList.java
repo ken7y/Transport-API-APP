aea0f442968d623ba0ca21414241ec78b48d08df
ken7y <kenwang2227@gmail.com>
2018-08-01 00:19:59 +1000

    transfered code from main to display routes

--- app/src/main/java/com/example/kenwang/dank/displayRoutesList.java
+++ app/src/main/java/com/example/kenwang/dank/displayRoutesList.java
@@ -2,7 +2,21 @@ package com.example.kenwang.dank;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import com.google.transit.realtime.GtfsRealtime;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class displayRoutesList extends AppCompatActivity {
 
@@ -12,16 +26,87 @@ public class displayRoutesList extends AppCompatActivity {
         setContentView(R.layout.activity_display_routes_list);
 
 
-
-        TextView textBox =(TextView) findViewById(R.id.startText);
+        TextView textBox = (TextView) findViewById(R.id.startText);
         String startText = getIntent().getExtras().getString("startLocation");
         textBox.setText(startText);
 
 
-        TextView endText =(TextView) findViewById(R.id.goalText);
+        TextView endText = (TextView) findViewById(R.id.goalText);
         String goalText = getIntent().getExtras().getString("destinationLocation");
         endText.setText(goalText);
 
+        final List<GtfsRealtime.FeedEntity> citylist = new ArrayList<>();
+        final List<GtfsRealtime.FeedEntity> macqlist = new ArrayList<>();
+        final List<GtfsRealtime.FeedEntity> fakelist = new ArrayList<>();
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                //.url("https://api.transport.nsw.gov.au/v1/gtfs/vehiclepos/buses")
+                .url("https://api.transport.nsw.gov.au/v1/gtfs/realtime/buses")
+
+                .header("Authorization", "apikey Qc9idalrWCIhYSKgNA0AVDFYXFOuaStWG66W")
+                .build();
+
+        Response response = null;
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    throw new IOException("Unexpected code " + response);
+                } else {
+                    // do something wih the result
+                    GtfsRealtime.FeedMessage newMessage = GtfsRealtime.FeedMessage.parseFrom(response.body().byteStream());
+                    int counting = 0;
+                    int total = 0;
+
+                    for (GtfsRealtime.FeedEntity testers : newMessage.getEntityList()) {
+                        String mainid = testers.getId();
+                        total++;
+                        String[] idafter = mainid.split("_");
+
+                        int indexOfLastStop = testers.getTripUpdate().getStopTimeUpdateList().size();
+
+                        counting++;
+
+                        if (testers.getTripUpdate().getStopTimeUpdate(indexOfLastStop - 1).getStopId().equals("200059")) {
+                            for (GtfsRealtime.TripUpdate.StopTimeUpdate temp : testers.getTripUpdate().getStopTimeUpdateList()) {
+                                if (temp.getStopId().equals("211220")) {
+                                    citylist.add(testers);
+                                }
+                            }
+
+                        } else if (testers.getTripUpdate().getStopTimeUpdate(indexOfLastStop - 1).getStopId().equals("211316")) {
+                            macqlist.add(testers);
+                        } else {
+                            fakelist.add(testers);
+
+                        }
+
+
+                        /**
+                         * if the last id is 200059 then its going city direction
+                         * if the last id is 211316 or 2113232 then its going macq uni direction
+                         */
+
+                    }
+/**
+ * can sometimes get two identical responses but one with delay and one without.
+ * Delay is in seconds
+ *
+ */
+
+                    Log.d("koolkids", "count");
+                    //Log.d("dank", response.body().bytes().length+"");
+                }
+            }
+        });
 
     }
 }
