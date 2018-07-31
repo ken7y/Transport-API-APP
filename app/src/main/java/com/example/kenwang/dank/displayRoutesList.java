07f09349e933e2958ad69f82ebe0d21c6d5ab6cf
ken7y <kenwang2227@gmail.com>
2018-08-01 01:45:30 +1000

    fixed bug its lit, like lamost there my dudes

--- app/src/main/java/com/example/kenwang/dank/displayRoutesList.java
+++ app/src/main/java/com/example/kenwang/dank/displayRoutesList.java
@@ -12,6 +12,7 @@ import com.google.transit.realtime.GtfsRealtime;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import okhttp3.Call;
@@ -30,7 +31,11 @@ public class displayRoutesList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_routes_list);
+        listViewId = (ListView) findViewById(R.id.listOfTimes);
 
+        adapter = new ArrayAdapter(displayRoutesList.this, android.R.layout.simple_list_item_1, lis5t);
+        listViewId.setAdapter(adapter);
+        Log.d("super dnak memes", "count");
 
         TextView textBox = (TextView) findViewById(R.id.startText);
         String startText = getIntent().getExtras().getString("startLocation");
@@ -41,18 +46,98 @@ public class displayRoutesList extends AppCompatActivity {
         String goalText = getIntent().getExtras().getString("destinationLocation");
         endText.setText(goalText);
 
+        final List<GtfsRealtime.FeedEntity> citylist = new ArrayList<>();
+        final List<GtfsRealtime.FeedEntity> macqlist = new ArrayList<>();
+        final List<GtfsRealtime.FeedEntity> fakelist = new ArrayList<>();
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                //.url("https://api.transport.nsw.gov.au/v1/gtfs/vehiclepos/buses")
+                .url("https://api.transport.nsw.gov.au/v1/gtfs/realtime/buses")
+
+                .header("Authorization", "apikey Qc9idalrWCIhYSKgNA0AVDFYXFOuaStWG66W")
+                .build();
+
+        Response response = null;
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    throw new IOException("Unexpected code " + response);
+                } else {
+                    // do something wih the result
+                    GtfsRealtime.FeedMessage newMessage = GtfsRealtime.FeedMessage.parseFrom(response.body().byteStream());
+                    int counting = 0;
+                    int total = 0;
+
+                    for (final GtfsRealtime.FeedEntity testers : newMessage.getEntityList()) {
+                        String mainid = testers.getId();
+                        total++;
+                        String[] idafter = mainid.split("_");
+
+                        int indexOfLastStop = testers.getTripUpdate().getStopTimeUpdateList().size();
+
+                        counting++;
+
+                        if (testers.getTripUpdate().getStopTimeUpdate(indexOfLastStop - 1).getStopId().equals("200059")) {
+                            for (GtfsRealtime.TripUpdate.StopTimeUpdate temp : testers.getTripUpdate().getStopTimeUpdateList()) {
+                                if (temp.getStopId().equals("211220")) {
+                                    citylist.add(testers);
+                                    Log.d("koolkids", "count");
+
+                                    runOnUiThread(new Runnable() {
+                                        public void run() {
+                                            long datee = testers.getTripUpdate().getStopTimeUpdate(41).getDeparture().getTime();
+                                            Date d = new Date(testers.getTripUpdate().getStopTimeUpdate(41).getDeparture().getTime()*1000);
+                                            lis5t.add(d);
+
+                                            adapter.notifyDataSetChanged();                                        }
+                                    });
+
+                                }
+                            }
+
+                        } else if (testers.getTripUpdate().getStopTimeUpdate(indexOfLastStop - 1).getStopId().equals("211316")) {
+                            macqlist.add(testers);
+                        } else {
+                            fakelist.add(testers);
+
+                        }
 
 
-        listViewId = (ListView) findViewById(R.id.listOfTimes);
+
+                        /**
+                         * if the last id is 200059 then its going city direction
+                         * if the last id is 211316 or 2113232 then its going macq uni direction
+                         */
+
+                    }
+/**
+ * can sometimes get two identical responses but one with delay and one without.
+ * Delay is in seconds
+ *
+ */
+
+                    //Log.d("dank", response.body().bytes().length+"");
+                }
+            }
+
+        });
+
 
         lis5t.add("testing");
         lis5t.add("testing");
         lis5t.add("testing");
         lis5t.add("testing");
+        Log.d("testing", "testing");
 
-        adapter = new ArrayAdapter(displayRoutesList.this, android.R.layout.simple_list_item_1, lis5t);
-        listViewId.setAdapter(adapter);
-        Log.d("super dnak memes", "count");
 
     }
 }
--- app/src/main/java/com/example/kenwang/dank/displayTransportList.java
+++ app/src/main/java/com/example/kenwang/dank/displayTransportList.java
@@ -80,10 +80,6 @@ public class displayTransportList extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 destinationLocation = "TownHall";
-                List<GtfsRealtime.FeedEntity> returned = testFunc();
-
-
-
                 Intent startIntent = new Intent(getApplicationContext(), displayRoutesList.class);
                 startIntent.putExtra("startLocation", startLocation);
                 startIntent.putExtra("destinationLocation", destinationLocation);
@@ -120,87 +116,6 @@ public class displayTransportList extends AppCompatActivity {
 
     }
 
-
-    public List<GtfsRealtime.FeedEntity> testFunc() {
-
-        final List<GtfsRealtime.FeedEntity> citylist = new ArrayList<>();
-        final List<GtfsRealtime.FeedEntity> macqlist = new ArrayList<>();
-        final List<GtfsRealtime.FeedEntity> fakelist = new ArrayList<>();
-
-        OkHttpClient client = new OkHttpClient();
-
-        Request request = new Request.Builder()
-                //.url("https://api.transport.nsw.gov.au/v1/gtfs/vehiclepos/buses")
-                .url("https://api.transport.nsw.gov.au/v1/gtfs/realtime/buses")
-
-                .header("Authorization", "apikey Qc9idalrWCIhYSKgNA0AVDFYXFOuaStWG66W")
-                .build();
-
-        Response response = null;
-
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                e.printStackTrace();
-            }
-
-            @Override
-            public void onResponse(Call call, final Response response) throws IOException {
-                if (!response.isSuccessful()) {
-                    throw new IOException("Unexpected code " + response);
-                } else {
-                    // do something wih the result
-                    GtfsRealtime.FeedMessage newMessage = GtfsRealtime.FeedMessage.parseFrom(response.body().byteStream());
-                    int counting = 0;
-                    int total = 0;
-
-                    for (GtfsRealtime.FeedEntity testers : newMessage.getEntityList()) {
-                        String mainid = testers.getId();
-                        total++;
-                        String[] idafter = mainid.split("_");
-
-                        int indexOfLastStop = testers.getTripUpdate().getStopTimeUpdateList().size();
-
-                        counting++;
-
-                        if (testers.getTripUpdate().getStopTimeUpdate(indexOfLastStop - 1).getStopId().equals("200059")) {
-                            for (GtfsRealtime.TripUpdate.StopTimeUpdate temp : testers.getTripUpdate().getStopTimeUpdateList()) {
-                                if (temp.getStopId().equals("211220")) {
-                                    citylist.add(testers);
-
-
-                                }
-                            }
-
-                        } else if (testers.getTripUpdate().getStopTimeUpdate(indexOfLastStop - 1).getStopId().equals("211316")) {
-                            macqlist.add(testers);
-                        } else {
-                            fakelist.add(testers);
-
-                        }
-
-
-                        /**
-                         * if the last id is 200059 then its going city direction
-                         * if the last id is 211316 or 2113232 then its going macq uni direction
-                         */
-
-                    }
-/**
- * can sometimes get two identical responses but one with delay and one without.
- * Delay is in seconds
- *
- */
-
-                    Log.d("koolkids", "count");
-                    //Log.d("dank", response.body().bytes().length+"");
-                }
-            }
-        });
-
-
-        return citylist;
-    }
 
 }
 
